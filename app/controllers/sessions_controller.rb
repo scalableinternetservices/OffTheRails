0842c21f2200afe2b1a94004743fccd2dee0cdca
Vivian Ross <vivianross06@users.noreply.github.com>
2022-11-03 23:03:21 -0700

    i hate linting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,47 +1,49 @@
+# frozen_string_literal: true
 class SessionsController < ApplicationController
-    def create
-        @user = User.find_by(email: session_params[:email])
-        puts "AAAAAAAHHHHHH"
-        puts "LOGGING IN"
-        puts @user.inspect
+
+  def create
+      @user = User.find_by(email: session_params[:email])
+      puts @user.inspect
       
-        if @user && @user.authenticate(session_params[:password])
-          login!
-          puts "logged in"
+      if @user && @user.authenticate(session_params[:password])
+        login!
           render json: {
             logged_in: true,
             user: @user
-          }
-        else
-            puts "didn't log in"
+        }
+      else
           render json: { 
-            status: 401,
-            errors: ['no such user, please try again']
-          }
-        end
-    end
-    def is_logged_in?
-        if logged_in? && current_user
-          render json: {
-            logged_in: true,
-            user: current_user
-          }
-        else
-          render json: {
-            logged_in: false,
-            message: 'no such user'
-          }
-        end
-    end
-    def destroy
-          logout!
-          render json: {
-            status: 200,
-            logged_out: true
-          }
-    end
-    private
-    def session_params
-          params.require(:session).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-    end
-    end
+          status: 401,
+          errors: ['no such user, please try again']
+        }
+      end
+  end
+
+  def is_logged_in?
+      if logged_in? && current_user
+        render json: {
+          logged_in: true,
+          user: current_user
+        }
+      else
+        render json: {
+          logged_in: false,
+          message: 'no such user'
+        }
+      end
+  end
+
+  def destroy
+        logout!
+        render json: {
+          status: 200,
+          logged_out: true
+        }
+  end
+
+  private
+
+  def session_params
+        params.require(:session).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+end
