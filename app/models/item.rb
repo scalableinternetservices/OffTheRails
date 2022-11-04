72c8dee4ae74854595b0316dc673df1a24817ebd
Sydney Lim <sydneylim@ucsb.edu>
2022-11-03 21:54:35 -0700

    sl - undo changes to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
-  belongs_to :user, foreign_key: 'user_id'
+  belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
   has_many :reviews
 end
