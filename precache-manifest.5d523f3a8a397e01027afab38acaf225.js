a8c09b8acbafbca87308ac6fa1adab8e6600885a
Justin Chang <changguangyu@yahoo.com>
2022-11-04 10:54:16 -0700

    Updates

---
+++ precache-manifest.5d523f3a8a397e01027afab38acaf225.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfcf716be6408debfb643aa8d0473b5",
+    "url": "/OffTheRails/index.html"
+  },
+  {
+    "revision": "452e089b9131ef1641d2",
+    "url": "/OffTheRails/static/css/main.bd6cfa8a.chunk.css"
+  },
+  {
+    "revision": "06185ad68bd1b0f53a32",
+    "url": "/OffTheRails/static/js/2.f8bea1e4.chunk.js"
+  },
+  {
+    "revision": "59ff913b357710c4255b5f74c8959408",
+    "url": "/OffTheRails/static/js/2.f8bea1e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452e089b9131ef1641d2",
+    "url": "/OffTheRails/static/js/main.479990e4.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/OffTheRails/static/js/main.479990e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a3192013d00fc850fd",
+    "url": "/OffTheRails/static/js/runtime-main.b85d79b4.js"
+  }
+]);
