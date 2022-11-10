2c0d39447dc2f913d1a525615498029129496175
Sydney Lim <sydneylim@ucsb.edu>
2022-11-09 21:29:18 -0800

    sl - remove extra route

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
-  get '/items/rating/:id', to: 'items#show_ratings'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
