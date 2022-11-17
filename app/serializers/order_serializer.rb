25ba30d7812c3dd18bdc88f697857437bcb0ac86
Vivian Ross <vivianross06@users.noreply.github.com>
2022-11-17 14:41:13 -0800

    vr - displays table of purchased orders on order page

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :purchased, :user_id
+  attributes :id, :purchased, :user_id, :updated_at
   has_many :order_items
 end
