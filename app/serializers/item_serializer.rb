6883824046ef50a97a733875ee351eadcbb367f1
Justin Chang <changguangyu@yahoo.com>
2022-12-03 20:51:45 -0800

    Add eager loading for has_many

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -3,4 +3,8 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :price, :image_link, :seller_id, :quantity, :show
   has_many :ratings
+
+  def ratings
+    object.ratings.includes(:user)
+  end
 end
--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -3,4 +3,8 @@
 class OrderSerializer < ActiveModel::Serializer
   attributes :id, :purchased, :user_id, :updated_at
   has_many :order_items
+
+  def order_items
+    object.order_items.includes(:item)
+  end
 end
